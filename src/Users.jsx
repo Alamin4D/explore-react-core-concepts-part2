4d51a5ed3a67f230e274a03d78828a491099bbde
Alamin4D <alaminahmed79000@gmail.com>
2024-03-09 08:58:40 +0600

    explore events, state, use effect

---
+++ src/Users.jsx
@@ -0,0 +1,22 @@
+import './Users.css'
+import { useEffect, useState } from "react"
+import User from "./User";
+
+export default function Users () {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(data => setUsers(data))
+    }, [])
+
+    return (
+        <div className='box'>
+            <h1>Users: {users.length}</h1>
+            {
+                users.map(user => <User user={user}></User>)
+            }
+        </div>
+    )
+}
